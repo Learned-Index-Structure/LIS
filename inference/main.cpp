40e6edb7cf030346bcdaaeaaa191a09c75f16736
Umair <omerjerk@gmail.com>
2019-10-09 14:39:09 -0400

    average time over multiple iterations
    Signed-off-by: Umair <omerjerk@gmail.com>

--- inference/main.cpp
+++ inference/main.cpp
@@ -8,8 +8,10 @@ using namespace std;
 
 typedef chrono::high_resolution_clock Clock;
 
+#define PREDICT_ITER 1000ll
+
 template<size_t rows, size_t mid, size_t cols>
-void mm_normal(double (&A)[rows][mid], double (&B)[mid][cols], double (&C)[rows][cols]) {
+void mm_normal(int (&A)[rows][mid], int (&B)[mid][cols], int (&C)[rows][cols]) {
     for (int z = 0; z < rows; ++z) {
         for (int j = 0; j < cols; ++j) {
             for (int o = 0; o < mid; ++o) {
@@ -20,7 +22,7 @@ void mm_normal(double (&A)[rows][mid], double (&B)[mid][cols], double (&C)[rows]
 }
 
 template<size_t rows, size_t cols>
-void get_weight(double (&array)[rows][cols]) {
+void get_weight(int (&array)[rows][cols]) {
     for (size_t i = 0; i < rows; ++i) {
         for (size_t j = 0; j < cols; ++j) {
             array[i][j] = rand() % 10 + 1.0;
@@ -29,7 +31,7 @@ void get_weight(double (&array)[rows][cols]) {
 }
 
 template<size_t rows, size_t cols>
-void print_matrix(double (&array)[rows][cols]) {
+void print_matrix(int (&array)[rows][cols]) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             std::cout << array[i][j] << '\t';
@@ -39,7 +41,7 @@ void print_matrix(double (&array)[rows][cols]) {
 }
 
 template<size_t models, size_t rows, size_t cols>
-void load_layer_data(double (&layer_data)[models][rows][cols]) {
+void load_layer_data(int (&layer_data)[models][rows][cols]) {
     for (int i = 0; i < models; i++) {
         for (int j = 0; j < rows; j++) {
             for (int k = 0; k < cols; k++) {
@@ -50,7 +52,7 @@ void load_layer_data(double (&layer_data)[models][rows][cols]) {
 }
 
 template<size_t rows, size_t cols>
-void relu(double (&data)[rows][cols]) {
+void relu(int (&data)[rows][cols]) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             data[i][j] = (((unsigned int) data[i][j] & 0x80000000) != 0) ? data[i][j] : 0;
@@ -62,45 +64,44 @@ void relu(double (&data)[rows][cols]) {
 #define N 1000000000
 
 void eval() {
-    double key[1][1] = {{4.0}};
+    int key[1][1] = {{4}};
 
     //MM from first hidden layer output
-    double out_1[1][32] = {{0.0}};
-    double out_2[1][32] = {{0.0}};
+    int out_1[1][32] = {{0}};
+    int out_2[1][32] = {{0}};
 
-    double hidden_layer_1[1][32] = {{0.0}};
-    double hidden_layer_2[32][32] = {{0.0}};
-    double output_layer[32][1] = {{0.0}};
+    int hidden_layer_1[1][32] = {{0}};
+    int hidden_layer_2[32][32] = {{0}};
+    int output_layer[32][1] = {{0}};
 
     get_weight(hidden_layer_1);
     get_weight(hidden_layer_2);
     get_weight(output_layer);
 
     //Linear regression weight and bias
-    double lr_wt[2] = {3.42, 1.44};
+    int lr_wt[2] = {3, 1};
 
-    double layer_data[models][32][1] = {{{0}}};
+    int layer_data[models][32][1] = {{{0}}};
     load_layer_data(layer_data);
 
-    double pred_layer_1[1][1] = {{0.0}};
-
-//    unsigned long long tstart = __rdtsc();
+    int pred_layer_1[1][1] = {{0}};
 
     auto t1 = Clock::now();
-    mm_normal(key, hidden_layer_1, out_1);
-    relu(out_1);
-    mm_normal(out_1, hidden_layer_2, out_2);
-    relu(out_2);
-    mm_normal(out_2, output_layer, pred_layer_1);
-    relu(pred_layer_1);
+    for (int i = 0; i < PREDICT_ITER; ++i) {
+        mm_normal(key, hidden_layer_1, out_1);
+        relu(out_1);
+        mm_normal(out_1, hidden_layer_2, out_2);
+        relu(out_2);
+        mm_normal(out_2, output_layer, pred_layer_1);
+        relu(pred_layer_1);
+    }
 
     int pred = pred_layer_1[0][0] * models / N;
 
     double value = pred * lr_wt[0] + lr_wt[1];
     auto t2 = Clock::now();
-//    unsigned long long tend = __rdtsc();
     std::cout << "Time: "
-              << chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()
+              << chrono::duration<int64_t, std::nano>(t2 - t1).count()/PREDICT_ITER
               << " nanoseconds" << std::endl;
 
     std::cout << "Key, Value: " << key[0][0] << ", " << value << std::endl;
@@ -108,7 +109,9 @@ void eval() {
 }
 
 int main(int argc, char *argv[]) {
-    // eval();
+    eval();
+
+    exit(0);
 
     if (argc != 1) {
         cout << "Usage:" << endl;
