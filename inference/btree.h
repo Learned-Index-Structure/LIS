8a19f308e1058e8420d4cae616864e61af9af1e5
dr1990 <deepak.ranjan1990@gmail.com>
2019-11-11 17:11:58 -0500

    working code

--- inference/btree.h
+++ inference/btree.h
@@ -5,19 +5,19 @@
 #include <vector>
 
 using namespace std;
-using tree_type = btree::btree_map<float, uint32_t>;
+using tree_type = btree::btree_map<double, uint32_t>;
 
 inline
-uint32_t btree_find(const tree_type &tree, float key) {
+uint32_t btree_find(const tree_type &tree, double key) {
     uint32_t pos = tree.find(key).position;
     return ((*(tree.find(key).node)).value(pos)).second;
 }
 
 inline
-void btree_insert(tree_type &tree, const vector<float> &keys,
+void btree_insert(tree_type &tree, const vector<double> &keys,
                   const vector<uint32_t> &values, uint32_t start_index, uint32_t end_index) {
     for (uint32_t i = start_index; i < end_index; i++) {
-        tree.insert(std::pair<float, uint32_t>(keys[i], values[i]));
+        tree.insert(std::pair<double, uint32_t>(keys[i], values[i]));
     }
 }
 
--- inference/inference.cpp
+++ inference/inference.cpp
@@ -28,8 +28,8 @@ float solveFirstLayer(const Mat1x32 &hidden_layer_1, const Mat32x32 &hidden_laye
 
 inline
 pair<int, float> solveSecondLayer(const float &firstLayerOutput, const float &key,
-                                const vector<pair<float, float> > &linearModels, const float &N, vector<bool> isModel,
-                                const unordered_map<int, tree_type> &btreeMap) {
+                                  const vector<pair<float, float> > &linearModels, const float &N, vector<bool> isModel,
+                                  const unordered_map<int, tree_type> &btreeMap) {
     float temp = firstLayerOutput * linearModels.size() / N;
     int modelIndex = (int) temp; //floor
     cout << "modelIndex = " << modelIndex << endl;
@@ -50,20 +50,24 @@ int main(int argc, char **argv) {
 
     double offset;
     int dataLines;
-    vector<int> indices;
-    vector<float> data;
+    vector<uint32_t> indices;
+    vector<double> data;
     float temp1, temp2, temp3;
-    int tempInt1, tempInt2;
+    uint32_t tempInt1, tempInt2;
     double tempDouble;
+
+    vector<double, uint32_t> keyList;
 
     ifstream dataFile(argv[1]);
     if (dataFile.is_open()) {
         dataFile >> dataLines;
         for (int i = 0; i < dataLines; ++i) {
-
-//            if(i > 10000000) break;
             dataFile >> tempInt1;
             dataFile >> tempDouble;
+            
+            if (i % 10 == 0)
+                keyList.push_back(tempDouble, tempInt1);
+
             if (indices.size() == 0) {
                 offset = tempDouble;
             } else {
@@ -77,13 +81,17 @@ int main(int argc, char **argv) {
         }
     }
     dataFile.close();
+    cout << "offset: " << offset << endl;
 
     Mat1x32 hidden_layer_1;
     Mat32x32 hidden_layer_2;
     Mat1x32 output_layer;
     Mat1x32 key;
-    key.m[0][0] = 1427781637776.1 - offset; //TODO: randomly test for multiple keys
-    float keyToSearch = key.m[0][0];
+//    key.m[0][0] = 1427781637776.1 - offset; //TODO: randomly test for multiple keys
+    key.m[0][0] = 1427787916750.0 - offset;
+
+//    float keyToSearch = key.m[0][0];
+    double keyToSearch = 1427787916750.0 - offset;
 
     ifstream firstLayerWeightsFile(argv[2]);
     if (firstLayerWeightsFile.is_open()) {
@@ -126,17 +134,17 @@ int main(int argc, char **argv) {
     float firstLayerAns = solveFirstLayer(hidden_layer_1, hidden_layer_2, output_layer, key);
     cout << "first layer ans = " << firstLayerAns << endl;
     pair<int, float> secondLayerAns = solveSecondLayer(firstLayerAns, keyToSearch, linearModels, data.size(), isModel,
-                                                     btreeMap);
+                                                       btreeMap);
     cout << "second layer ans = " << secondLayerAns.second << endl;
 
     cout << "threshold = " << threshold << endl;
 
     if (isModel[secondLayerAns.first]) {
-        int positionOfKey = binarySearchBranchless<float>(data, keyToSearch, secondLayerAns.second, threshold);
+        int positionOfKey = binarySearchBranchless<double>(data, keyToSearch, secondLayerAns.second, threshold);
         cout << "position of key = " << positionOfKey << " value = " << data[positionOfKey] << endl;
     } else {
         tree_type btree;
-        vector<float> keys;
+        vector<double> keys;
         vector<uint32_t> values;
 
         auto startIndex = (uint32_t) (secondLayerAns.second + errors[secondLayerAns.first].first);
@@ -154,9 +162,11 @@ int main(int argc, char **argv) {
         btree_insert(btree, keys, values, 0, keys.size());
 
         for (uint32_t i = startIndex; i <= endIndex; i++) {
-            cout << (uint32_t) data[i] << " " << indices[i] << endl;
+            printf("%f1000.0 ", data[i]);
+            cout << (double) data[i] << " " << indices[i] << endl;
         }
         cout << "key is at position: " << (uint32_t) keyToSearch << ", " << btree_find(btree, keyToSearch) << endl;
+        printf("KeyToSearch: %f1000.0", keyToSearch);
     }
 
     return 0;
